1eb0d82d7dae22cd4898634a971dfed7f3c6d1b0
edsUlalan <edds.ulalan@gmail.com>
2021-09-12 08:38:53 +0800

    common operators & examples

--- operators.kt
+++ operators.kt
@@ -3,3 +3,42 @@ var x = 100 + 50
 // Operand -> 100
 // Operator -> +
 // Operator -> 50
+
+var sum1 = 100 + 50
+var sum2 = sum1 + 250
+var sum2 = sum2 + sum2
+
+// + -> addition
+// - -> subtraction
+// * -> multiplication
+// / -> division
+// % -> modulus
+// ++ -> increment
+// -- -> decrement
+
+// more examples
+
+var x = 10
+x += 5
+
+// x = 5 is same as x = 5
+// x += 3 is same as x = x + 3
+// x -= 3 is same as x = x - 3
+// x *= 3 is same as x = x * 3
+// x /= 3 is same as x = x / 3
+// x %= 3 is same as x = x % 3
+
+// comparison Operator
+
+// "Equal to" -> x == y
+// "Not Equal" -> x != y
+// "Greater than" -> x > y
+// "Less than" -> x < y
+// "Greater than or equal to" -> x >= y
+// "Less than or equal to" -> x <= y
+
+// Logical Operator
+
+// "Logical and" -> && - returns true if both statements are true
+// "Logical or" -> || - returns true if one of the statements is true
+// "Logical not" -> ! - reverse the result, returns false if the result is true
