25278fd3e5bb2334a556dc4b202d3bebc1d3712d
edsUlalan <edds.ulalan@gmail.com>
2021-09-13 11:39:19 +0800

    how to use boolean expressions

---
+++ booleans.kt
@@ -0,0 +1,22 @@
+val isKotlinFun: Boolean = true
+val isFishTasty: Boolean = false
+println(isKotlinFun) // outputs true
+println(isFishTasty) // outputs false
+
+// or
+
+val isKotlinFun = true
+val isFishTasty = false
+println(isKotlinFun)   // Outputs true
+println(isFishTasty)   // Outputs false
+
+// boolean expression
+val x = 10
+val y = 9
+println(x > y) // returns true, because 10 is greater than 9
+println(10 > 9) // returns true, because 10 is greater than 9
+
+val x = 10
+println(x == 10) // returns true, because the value of x is equal to 10
+
+println(10 == 15) // returns false, because 10 is not equal to 15
