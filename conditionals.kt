2c80613c9500f99c1ea5344a7c8dc55451cb0949
edsUlalan <edds.ulalan@gmail.com>
2021-09-13 11:46:13 +0800

    add conditional statements example if else

---
+++ conditionals.kt
@@ -0,0 +1,21 @@
+// if statement
+
+if (20 > 18) {
+  // block of code to be executed if the condition is true
+  println("20 is greater than 18")
+}
+
+val x = 20
+val y = 18
+if (x > y) {
+  println("x is greater than y")
+}
+
+// else statement
+
+val time = 20
+if (time < 18) {
+  println("Good day.")
+} else {
+  println("Good evening.")
+} // outputs "Good evening."
