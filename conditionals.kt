0c92051a1222bbdf071a35e2294c5f29cf2c7bb5
edsUlalan <edds.ulalan@gmail.com>
2021-09-14 15:12:40 +0800

    finish conditionals examples

--- conditionals.kt
+++ conditionals.kt
@@ -19,3 +19,32 @@ if (time < 18) {
 } else {
   println("Good evening.")
 } // outputs "Good evening."
+
+// else if statement
+
+val time = 22
+if (time < 10) {
+  println("Good morning.")
+} else if (time < 20) {
+  println("Good day.")
+} else {
+  println("Good evening.")
+} // outputs "Good evening."
+
+// if..else expression
+
+val time = 20
+val greeting = if (time < 18) {
+  "Good day."
+} else {
+  "Good evening."
+}
+println(greeting)
+
+// you can ommit the curly braces {} when if has only one statement
+
+fun main() {
+  val time = 20
+  val greeting = if (time < 18) "Good day." else "Good evening."
+  println(greeting)
+}
