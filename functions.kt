644731f38fe3b46234df16ab428ffca46038944c
edsUlalan <edds.ulalan@gmail.com>
2021-09-19 10:42:38 +0800

    update function examples

--- functions.kt
+++ functions.kt
@@ -40,3 +40,33 @@ fun main() {
 }
 
 // return values
+
+fun myFunction(x: Int): Int {
+  return (x + 5)
+}
+
+fun main() {
+  var result = myFunction(3)
+  println(result)
+} // 8 (3 + 5)
+
+fun myFunctionTwo(x: Int, y: Int): Int {
+  return (x + y)
+}
+
+fun mainTwo() {
+  var result = myFunction(3, 5)
+  println(result)
+} // 8 (3 + 5)
+
+// shorter syntax for return values
+// There is also a shorter syntax for returning values.
+// You can use the = operator instead of return without specifying the return type.
+// Kotlin is smart enough to automatically find out what it is
+
+fun myFunction(x: Int, y: Int) = x + y
+
+fun main() {
+  var result = myFunction(3, 5)
+  println(result)
+} // 8 (3 + 5)
